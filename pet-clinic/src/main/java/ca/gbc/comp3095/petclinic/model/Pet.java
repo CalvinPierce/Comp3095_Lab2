f45ae3e17cce1324eff24b70906803192d6fc531
Calvin Pierce <calvinpierce99@gmail.com>
2021-09-27 08:36:22 -0400

    Closes #2

---
+++ pet-clinic/src/main/java/ca/gbc/comp3095/petclinic/model/Pet.java
@@ -0,0 +1,10 @@
+package ca.gbc.comp3095.petclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+
+    private PetType petType;
+    private Owner owner;
+    private LocalDate localdate;
+}
