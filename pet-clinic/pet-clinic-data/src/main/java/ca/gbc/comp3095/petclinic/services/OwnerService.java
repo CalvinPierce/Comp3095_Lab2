e59def0f084cc1775d0e27c9b97dc3b8507fc89a
CalvinPierce <calvinpierce99@gmail.com>
2021-10-06 10:06:22 -0400

    Create PetClinic Services. Closes #15

---
+++ pet-clinic/pet-clinic-data/src/main/java/ca/gbc/comp3095/petclinic/services/OwnerService.java
@@ -0,0 +1,14 @@
+package ca.gbc.comp3095.petclinic.services;
+
+import ca.gbc.comp3095.petclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastname);
+    Owner findById(Long id);
+    Owner save(Owner owner);
+    Set<Owner> findAll();
+
+}
