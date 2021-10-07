2fe820ad99ef85b4fe166692f5d7b586bd77d032
CalvinPierce <77972370+CalvinPierce@users.noreply.github.com>
2021-10-07 17:34:46 -0400

    Delete Lecture 3 Directory
    Was not supposed to be in this repository

--- Lecture3/src/main/java/ca/gbc/comp3095/lecture3/services/GreetingService.java
+++
@@ -1,6 +0,0 @@
-package ca.gbc.comp3095.lecture3.services;
-
-public interface GreetingService {
-
-    String sayGreeting();
-}
--- Lecture3/src/main/java/ca/gbc/comp3095/lecture3/services/GreetingServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package ca.gbc.comp3095.lecture3.services;
-
-public class GreetingServiceImpl implements GreetingService{
-
-    @Override
-    public String sayGreeting() {
-        return "Hello World";
-    }
-}
