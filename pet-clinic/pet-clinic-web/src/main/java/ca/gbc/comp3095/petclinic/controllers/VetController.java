db4ac77d70acc59c4d0d47ecc7dcf7528309e00c
CalvinPierce <calvinpierce99@gmail.com>
2021-10-18 09:27:33 -0400

    Refactored VetController to list all vets on index page. Closes #19

--- pet-clinic/pet-clinic-web/src/main/java/ca/gbc/comp3095/petclinic/controllers/VetController.java
+++ pet-clinic/pet-clinic-web/src/main/java/ca/gbc/comp3095/petclinic/controllers/VetController.java
@@ -1,14 +1,24 @@
 package ca.gbc.comp3095.petclinic.controllers;
 
+import ca.gbc.comp3095.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vet")
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vet/index";
     }
 
