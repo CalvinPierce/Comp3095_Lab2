59160ab02b98804e29a8436f6ebf73cb76a6c007
CalvinPierce <calvinpierce99@gmail.com>
2021-10-18 09:23:55 -0400

    Refactored OwnerController to list all owners on index page. Closes #19

--- pet-clinic/pet-clinic-web/src/main/java/ca/gbc/comp3095/petclinic/controllers/OwnerController.java
+++ pet-clinic/pet-clinic-web/src/main/java/ca/gbc/comp3095/petclinic/controllers/OwnerController.java
@@ -1,14 +1,23 @@
 package ca.gbc.comp3095.petclinic.controllers;
 
+import ca.gbc.comp3095.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owner")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwner() {
+    public String listOwner(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owner/index";
     }
 
