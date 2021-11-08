7f9d5a22fd52117cb4f86e0d5de487b5deec9d2d
CalvinPierce <calvinpierce99@gmail.com>
2021-11-08 08:34:01 -0500

    Closes #29

--- pet-clinic/pet-clinic-data/src/main/java/ca/gbc/comp3095/petclinic/model/Owner.java
+++ pet-clinic/pet-clinic-data/src/main/java/ca/gbc/comp3095/petclinic/model/Owner.java
@@ -1,7 +1,16 @@
 package ca.gbc.comp3095.petclinic.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Owner extends Person{
-    private Set<Pet> pets;
+    private Set<Pet> pets = new HashSet<>();
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
