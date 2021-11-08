34b31964dc5c4f06e0d1ad51250574fb5a899232
CalvinPierce <calvinpierce99@gmail.com>
2021-11-08 09:05:34 -0500

    Closes #33

--- pet-clinic/pet-clinic-web/src/main/java/ca/gbc/comp3095/petclinic/bootstrap/DataLoader.java
+++ pet-clinic/pet-clinic-web/src/main/java/ca/gbc/comp3095/petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package ca.gbc.comp3095.petclinic.bootstrap;
 
 import ca.gbc.comp3095.petclinic.model.Owner;
+import ca.gbc.comp3095.petclinic.model.PetType;
 import ca.gbc.comp3095.petclinic.model.Vet;
 import ca.gbc.comp3095.petclinic.services.OwnerService;
+import ca.gbc.comp3095.petclinic.services.PetTypeService;
 import ca.gbc.comp3095.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType dogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType catPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Bruce");
