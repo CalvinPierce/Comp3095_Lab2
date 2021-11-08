2b57ef3b848b8481aa363e9488be9137629cd622
CalvinPierce <calvinpierce99@gmail.com>
2021-11-08 08:50:13 -0500

    Closes #31

---
+++ pet-clinic/pet-clinic-data/src/main/java/ca/gbc/comp3095/petclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,35 @@
+package ca.gbc.comp3095.petclinic.services.map;
+
+import ca.gbc.comp3095.petclinic.model.PetType;
+import ca.gbc.comp3095.petclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
